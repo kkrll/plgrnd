85842353931b66efc282b280bdccaefd3336c488
kkrll <k_kov@hotmail.com>
2025-03-12 15:37:57 +0100

    graph preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const pages: { url: string; name: string }[] = [
+    { url: "/graph", name: "Graph" },
+    { url: "/pricing", name: "Pricing" },
+    { url: "/chat", name: "Chat" },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-     <Link href="/chat">Chat</Link>
-     <Link href="/pricing">Pricing</Link>
+    <main className="flex min-h-screen flex-col items-start justify-center p-[30%] gap-4">
+      <h1 className="text-4xl font-bold mb-8">PLAYGROUND</h1>
+      {pages.map((page) => {
+        return (
+          <Link href={page.url} className="underline">
+            {page.name}
+          </Link>
+        );
+      })}
     </main>
   );
 }
