16caa57e871bf97cbec766b14a12bfa718b115b4
kkrll <k_kov@hotmail.com>
2026-01-26 15:15:34 +0100

    feedback

--- src/app/manFunnel/components/steps/FeedbackEarly.tsx
+++ src/app/manFunnel/components/steps/FeedbackEarly.tsx
@@ -7,6 +7,21 @@ import Logo from "../Logo";
 export default function NameStep() {
   const { nextStep, funnelData } = useFunnelContext();
 
+  const getAgeGroup = (age?: string) => {
+    switch (age) {
+      case ("18-29"):
+        return "under 30";
+      case ("30-39"):
+        return "in their 30s";
+      case ("40-49"):
+        return "in their 40s";
+      case ("50+"):
+        return "over 50";
+      default:
+        return "";
+    }
+  };
+
   return (
     <section className="w-full h-full p-6 min-h-screen flex flex-col">
       <div
@@ -19,12 +34,22 @@ export default function NameStep() {
       <div className="flex flex-1 flex-col justify-center items-center">
         <h2 className="text-center mb-6">
           {funnelData.name},{" "}
-          <span className="text-blue-300">over 500 000 men</span> in{" "}
-          <span className="text-blue-300">their 40s</span> have already tried{" "}
+          <span className="text-blue-300">over 500k men{" "}{getAgeGroup(funnelData.age)}</span> have already tried{" "}
           <br />
           Zing AI Coach.
         </h2>
-        <h2 className=" mb-6">Now it’s your turn.</h2>
+        <h2 className="">Now it’s your turn.</h2>
+
+        <div className="bg-white/10 w-1/2 h-[2px] my-8 mx-auto"></div>
+        <p className="text-center text-white/85 mb-4 uppercase text-sm font-semibold">Best AI Coach App according to</p>
+        <div className="flex gap-2 w-full">
+          <div className="flex-1 flex justify-center items-center bg-white/10 py-4 rounded-2xl backdrop-blur-md">
+            <img src="/man-funnel/feedback-early/tr.png" alt="Techradar" className="h-4" />
+          </div>
+          <div className="flex-1 flex justify-center items-center bg-white/10 py-4 rounded-2xl backdrop-blur-md">
+            <img src="/man-funnel/feedback-early/mh.svg" alt="Men's Health" className="h-4" />
+          </div>
+        </div>
       </div>
 
       <Button
--- src/app/manFunnel/components/steps/MuscleGainEase.tsx
+++ src/app/manFunnel/components/steps/MuscleGainEase.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useFunnelContext } from "../../context/FunnelContext";
-import { useDebouncedAction } from "../../hooks/useDebouncedAction";
+import FormOptions from "../FormOptions";
 
 const options = [
   { id: "hard", label: "Hard to gain muscle" },
@@ -11,32 +11,19 @@ const options = [
 
 export default function MuscleGainEase() {
   const { updateData, nextStep } = useFunnelContext();
-  const debouncedAction = useDebouncedAction({ delay: 300 });
 
-  const handleSelect = (id: string) => {
-    updateData({ muscleGainEase: id });
-    debouncedAction(nextStep);
+  const handleSubmit = (selected: string | string[]) => {
+    updateData({ muscleGainEase: selected as string });
+    nextStep();
   };
 
   return (
     <section className="w-full p-6 min-h-screen flex-1 flex flex-col">
-      <h2 className="text-2xl font-bold mb-8">
+      <h2 className="mb-8">
         How easily do you gain muscle?
       </h2>
 
-      <div className="flex flex-col gap-3">
-        {options.map((option) => (
-          <button
-            key={option.id}
-            onClick={() => handleSelect(option.id)}
-            className="pressable relative group rounded-2xl overflow-hidden bg-grey-800 hover:bg-grey-700 transition-all p-6 flex items-center justify-center"
-          >
-            <span className="text-white font-medium text-center">
-              {option.label}
-            </span>
-          </button>
-        ))}
-      </div>
+      <FormOptions options={options} type="radio" onSubmit={handleSubmit} />
     </section>
   );
 }
