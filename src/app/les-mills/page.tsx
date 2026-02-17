ffd90310cc80736d4fd28ae30df6bcbe83b7ddda
zingcoach <zingcoach@zingcoachs-MacBook-Pro.local>
2026-02-17 13:44:12 +0100

    border-radius fix

--- src/app/les-mills/page.tsx
+++ src/app/les-mills/page.tsx
@@ -5,7 +5,13 @@ import { useEffect, useState } from "react";
 import "./style.css";
 
 const imgs: number[] = Array.from({ length: 16 }, (_, i) => i + 1);
-type TileClass = "" | "grow-rb" | "grow-rt" | "grow-lb" | "grow-lt" | "overlayed";
+type TileClass =
+  | ""
+  | "grow-rb"
+  | "grow-rt"
+  | "grow-lb"
+  | "grow-lt"
+  | "overlayed";
 type GridState = Record<number, TileClass>;
 
 const gridMap: GridState[] = [
@@ -89,43 +95,73 @@ const LesMills = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIteration((prev) => prev < gridMap.length - 1 ? prev + 1 : 0);
+      setIteration((prev) => (prev < gridMap.length - 1 ? prev + 1 : 0));
     }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <section className="w-full max-w-[512px] mx-auto h-screen p-6 flex items-center justify-end flex-col gap-10 bg-grey-200">
-      <div className="w-full flex flex-col items-center gap-4">
-      <div className="lm-grid grid grid-cols-4 grid-rows-4 w-full gap-1">
-        {imgs.map((img) => {
-          const tileClass = activeGridMap[img] ?? "";
-          return (
-            <div key={img} className={`lm-cell col-span-1 row-span-1 ${tileClass}`}>
-              <Image
-                src={`/les-mills/${img}.webp`}
-                alt={`Les Mills ${img}`}
-                fill
-                sizes="25vw"
-                className="object-cover w-full h-full"
-              />
-            </div>
-          );
-        })}
+    <section className="w-full max-w-[512px] mx-auto h-dvh p-6 flex flex-col justify-between gap-6 bg-grey-200 overflow-hidden">
+      <div className="bg-white w-11 h-11 min-w-11 min-h-11 shrink-0 ml-auto flex items-center justify-center rounded-2xl">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.295367 0.295367C0.68919 -0.0984557 1.3277 -0.0984557 1.72153 0.295367L11.7046 10.2785C12.0985 10.6723 12.0985 11.3108 11.7046 11.7046C11.3108 12.0985 10.6723 12.0985 10.2785 11.7046L0.295367 1.72153C-0.0984557 1.3277 -0.0984557 0.68919 0.295367 0.295367Z"
+            fill="black"
+          />
+          <path
+            d="M11.7046 0.295367C12.0985 0.68919 12.0985 1.3277 11.7046 1.72153L1.72153 11.7046C1.3277 12.0985 0.68919 12.0985 0.295367 11.7046C-0.0984555 11.3108 -0.0984559 10.6723 0.295367 10.2785L10.2785 0.295367C10.6723 -0.0984557 11.3108 -0.0984557 11.7046 0.295367Z"
+            fill="black"
+          />
+        </svg>
       </div>
-      <img src="/les-mills/logo.svg" alt="Les Mills"/>
+      <div className="lm-middle w-full flex-1 min-h-0 flex flex-col items-center justify-center">
+        <div className="lm-middle-content flex flex-col justify-start">
+          <div className="lm-grid grid grid-cols-4 grid-rows-4 w-full aspect-square gap-1">
+            {imgs.map((img) => {
+              const tileClass = activeGridMap[img] ?? "";
+              return (
+                <div
+                  key={img}
+                  className={`lm-cell col-span-1 row-span-1 ${tileClass}`}
+                >
+                  <Image
+                    src={`/les-mills/${img}.webp`}
+                    alt={`Les Mills ${img}`}
+                    fill
+                    sizes="25vw"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="lm-logo-gradient">
+          <img src="/les-mills/logo.svg" alt="Les Mills" className="h-12" />
+        </div>
       </div>
 
-      <div className="flex flex-col gap-2 text-center mt-2">
-        <h1 className="text-3xl font-bold text-black">Join the Ultimate <br/>Fitness Experience</h1>
-        <p className="text-gray-500">
-          Unlock Les Mills premium library and enjoy studio-quality workouts at home.
+      <div className="flex flex-col gap-4 text-center mt-2 w-full shrink-0">
+        <h1 className="text-[32px] leading-10 font-bold text-black">
+          Open phenomenal
+          <br />
+          Less Mills library
+        </h1>
+        <p className="text-gray-500 mb-6">
+          Add 2000+ studio-quality workouts and over 25 fitness programs to your
+          fitness journey.
         </p>
+        <button className="bg-black text-white h-[56px] flex items-center justify-center px-4 py-3 rounded-2xl w-full">
+          Get Started
+        </button>
       </div>
-      <button className="bg-blue-500 text-white px-4 py-3 rounded-lg w-full">
-        Get Started
-      </button>
     </section>
   );
 };
